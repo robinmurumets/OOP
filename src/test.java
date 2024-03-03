698d88979a1c559e2a5c308eaa82659df768034b
Robin Murumets <99771771+robinmurumets@users.noreply.github.com>
2024-03-03 15:26:22 +0200

    Update test.java

--- src/test.java
+++ src/test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Muudatus MUUDATUS MUUDATUS");
+        System.out.println("Friend still here");
     }
 }
